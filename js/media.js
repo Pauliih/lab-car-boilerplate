8fa040c1aaabfb136c5f4b646aaa9f19f1b03ca4
Pauliih <pau.campos89@gmail.com>
2017-12-06 00:34:25 -0300

    menu and footer movile + modal-structure

---
+++ js/media.js
@@ -0,0 +1,12 @@
+@media (max-width: 768px) {
+    .white-margin {
+        border: 2px solid #fff;
+        margin-left: 10%;
+    }
+    .btn-bars {
+        float: left;
+    }
+    .logo-nav {
+        margin-left: 50%;
+    }
+}
